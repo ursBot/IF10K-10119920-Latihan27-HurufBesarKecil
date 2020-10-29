089afa60770cffe9ba734bd37d74db5230e62bf0
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-29 22:04:15 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan27/hurufbesarkecil/HurufBesarKecil.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan27.hurufbesarkecil;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class HurufBesarKecil {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        String kalimat, besar, kecil;
+        
+        System.out.print("Masukkan Kalimat : ");
+        kalimat = input.nextLine();
+        besar = kalimat.toUpperCase();
+        kecil = kalimat.toLowerCase();
+        System.out.println();
+        System.out.println("====Hasil Formatting====");
+        System.out.println("Huruf Besar : " + besar);
+        System.out.println("Huruf Kecil : " + kecil);
+    }
+}
